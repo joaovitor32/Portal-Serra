3b9ec9792ddf9097c3850687e894cc9da9694773
PedroHQCoelho <pedrohqcoelho@gmail.com>
2020-04-02 14:58:10 -0300

    origin

--- Frontend/pages/Index/Index.php
+++ Frontend/pages/Index/Index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <link rel='stylesheet' type='text/css' href='../../components/footer/footer.css'>
+    <link rel='stylesheet' type='text/css' href='../../components/form/styles.css'>
     <link rel='stylesheet' type='text/css' href='./Index.css'>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -16,6 +17,9 @@
 
 <body>
     <?php
+        require("../../components/form/form.php")
+    ?>
+    <?php    
         require("../../components/footer/footer.php")
     ?>
 </body>
