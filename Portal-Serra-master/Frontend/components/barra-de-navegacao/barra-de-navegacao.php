2feb8677ec4aa8e3a9670ae73fc34e33507691a5
matsales <50926779+matsales@users.noreply.github.com>
2020-04-08 18:38:36 -0300

    Add files via upload

---
+++ Portal-Serra-master/Frontend/components/barra-de-navegacao/barra-de-navegacao.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="barra-de-navegacao.css">
+    </head>
+    <body>
+        <nav id="menu-horizontal-index">
+            <ul>
+                <li><a href="#">[ Imagem ]</a></li>
+                <li><a href="#">Mensagem</a></li>
+                <li><a href="#">Portal</a></li>
+                <li><a href="#">Diretoria</a></li>
+            </ul>
+        </nav>
+    </body>
+</html>
---
+++ Portal-Serra-master/Frontend/components/form/form.php
@@ -0,0 +1,22 @@
+<form action="/" method="post">
+<div class="content">
+<div>
+    <div class="input-wrapper">
+        <label for="description">Descrição de atividades passadas e o sucesso obtido:</label>
+        <textarea name="description" cols="40" rows="10"></textarea>
+    </div>
+
+    <div class="input-wrapper">
+        <label for="plan">Plano de ação para a próxima quinzena:</label>
+        <textarea name="plan" cols="40" rows="10"></textarea>
+    </div>
+
+    <div class="input-wrapper">
+        <label for="obs">Observação:</label>
+        <textarea name="obs" cols="40" rows="10"></textarea>
+    </div>
+
+    <button>Enviar</button>
+</div>
+</div>
+</form>
