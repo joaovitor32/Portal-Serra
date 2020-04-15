104f7b971ab6dcdafc60cf07659ac0ac63911ee4
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-04-14 22:38:16 -0300

    Algumas mudanças nos controlles de Users e Respostas

--- api/controller/resposta.php
+++ api/controller/resposta.php
@@ -1,57 +1,56 @@
 <?php
-    $requestBody=json_decode(file_get_contents('php://input'));
-    $requestHeader=getallheaders();
+   function execute_action($acao,$requestBody,$requestHeader){
 
-    if(!empty($requestHeader)&&!empty($requestBody)){
+        if(!empty($acao)&&!empty($requestHeader)&&!empty($requestBody)){
 
-        switch($requestBody->action){
-            case "GET_RESPOSTAS":
-                include '../Models/resposta.php';
-                $resposta=new  Resposta();
-                $resposta->lista();
-                return;
-            break;
-            case "CREATE_RESPOSTA":
-                include '../Models/resposta.php';
-                $resposta=new Resposta();
-                $resposta->setQuestao1($requestBody->questao1);
-                $resposta->setQuestao2($requestBody->questao2);
-                $resposta->setObservacao($requestBody->observacao);
-                $resposta->setData($requestBody->data);
-                $resposta->create();
-                return;
-            break;
-            case "READ_RESPOSTA":
-                include "../Models/resposta.php";
-                $resposta=new Resposta();
-                $resposta->setIdResposta($requestBody->idResposta);
-                $resposta->read();
-                return;
-            break;
-            case "UPDATE_RESPOSTA":
-                include "../Models/resposta.php";
-                $resposta=new Resposta();
-                $resposta->setIdResposta($requestBody->idResposta);
-                $resposta->setQuestao1($requestBody->questao1);
-                $resposta->setQuestao2($requestBody->questao2);
-                $resposta->setObservacao($requestBody->observacao);
-                $resposta->setData($requestBody->data);
-                $resposta->update();
-                return;
-            break;
-            case "DELETE_RESPOSTA":
-                include "../Models/resposta.php";
-                $resposta=new Resposta();
-                $resposta->setIdResposta($requestBody->idResposta);
-                $resposta->delete();
-                return;
-            break;
-            default:
-                http_response_code(400);
-                echo "Operação inválida";
-                return;
-        }
+            switch($acao){
+                case "GET_RESPOSTAS":
+                    include '../Models/resposta.php';
+                    $resposta=new  Resposta();
+                    $resposta->lista();
+                    return;
+                break;
+                case "CREATE_RESPOSTA":
+                    include '../Models/resposta.php';
+                    $resposta=new Resposta();
+                    $resposta->setQuestao1($requestBody->questao1);
+                    $resposta->setQuestao2($requestBody->questao2);
+                    $resposta->setObservacao($requestBody->observacao);
+                    $resposta->setData($requestBody->data);
+                    $resposta->create();
+                    return;
+                break;
+                case "READ_RESPOSTA":
+                    include "../Models/resposta.php";
+                    $resposta=new Resposta();
+                    $resposta->setIdResposta($requestBody->idResposta);
+                    $resposta->read();
+                    return;
+                break;
+                case "UPDATE_RESPOSTA":
+                    include "../Models/resposta.php";
+                    $resposta=new Resposta();
+                    $resposta->setIdResposta($requestBody->idResposta);
+                    $resposta->setQuestao1($requestBody->questao1);
+                    $resposta->setQuestao2($requestBody->questao2);
+                    $resposta->setObservacao($requestBody->observacao);
+                    $resposta->setData($requestBody->data);
+                    $resposta->update();
+                    return;
+                break;
+                case "DELETE_RESPOSTA":
+                    include "../Models/resposta.php";
+                    $resposta=new Resposta();
+                    $resposta->setIdResposta($requestBody->idResposta);
+                    $resposta->delete();
+                    return;
+                break;
+                default:
+                    http_response_code(400);
+                    echo "Operação inválida";
+                    return;
+            }
 
     }
-
+}
 ?>
--- api/controller/users.php
+++ api/controller/users.php
@@ -1,61 +1,59 @@
 <?php
-$requestBody = json_decode(file_get_contents("php://input"));
-$requestHeaders = getallheaders();
 
-if (!empty($requestBody) && !empty($requestHeaders)) {
+function execute_action($acao,$requestBody,$requestHeaders){
+    if (!empty($acao) &&!empty($requestBody) && !empty($requestHeaders)) {
+        include("../Models/token.php");
+        $token = new Token();
+        $token->setToken($requestHeaders);
 
-
-    include("../Models/token.php");
-    $token = new Token();
-    $token->setToken($requestHeaders);
-
-    switch ($data->action) {
-        case "CADASTRO_USUARIO":
-            include '../Models/users.php';
-            $user = new User();
-            $user->setFull_Name($requestBody->full_name);
-            $user->setCargo($requestBody->cargo);
-            $user->setSenha(md5($requestBody->senha));
-            $user->setEntrada($requestBody->entrada);
-            $user->create();
-            return;
-            break;
-        case "GET_USERS":
-            include "../Models/users";
-            $user = new User();
-            $user->lista();
-            return;
-            break;
-        case "GET_USER":
-            include '../Models/users';
-            $user = new User();
-            $user->setCodUser($requestBody->codUser);
-            $user->read();
-            return;
-            break;
-        case "UPDATE_USER":
-            include '../Models/users';
-            $user = new User();
-            $user->setCodUser($requestBody->codUser);
-            $user->setSenha(md5($requestBody->senha));
-            $user->setFull_Name($requestBody->full_name);
-            $user->setCargo($requestBody->cargo);
-            $user->setEntrada($requestBody->entrada);
-            $user->update();
-            return;
-            break;
-        case "DELETE_USER":
-            if ($token->VerificaJWT()) {
+        switch ($acao) {
+            case "CADASTRO_USUARIO":
+                include '../Models/users.php';
+                $user = new User();
+                $user->setFull_Name($requestBody->full_name);
+                $user->setCargo($requestBody->cargo);
+                $user->setSenha(md5($requestBody->senha));
+                $user->setEntrada($requestBody->entrada);
+                $user->create();
+                return;
+                break;
+            case "GET_USERS":
+                include "../Models/users";
+                $user = new User();
+                $user->lista();
+                return;
+                break;
+            case "GET_USER":
                 include '../Models/users';
                 $user = new User();
                 $user->setCodUser($requestBody->codUser);
-                $user->delete();
+                $user->read();
+                return;
+                break;
+            case "UPDATE_USER":
+                include '../Models/users';
+                $user = new User();
+                $user->setCodUser($requestBody->codUser);
+                $user->setSenha(md5($requestBody->senha));
+                $user->setFull_Name($requestBody->full_name);
+                $user->setCargo($requestBody->cargo);
+                $user->setEntrada($requestBody->entrada);
+                $user->update();
+                return;
+                break;
+            case "DELETE_USER":
+                if ($token->VerificaJWT()) {
+                    include '../Models/users';
+                    $user = new User();
+                    $user->setCodUser($requestBody->codUser);
+                    $user->delete();
+                    return;
+                }
+                break;
+            default:
+                http_response_code(400);
+                echo "Operação inválida";
                 return;
             }
-            break;
-        default:
-            http_response_code(400);
-            echo "Operação inválida";
-            return;
     }
 }
