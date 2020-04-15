d86873d3bc80f389adda9a8705d9678cc92d734f
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-04-14 22:39:30 -0300

    Rotas resposta

---
+++ api/routes/respostas/read.php
@@ -0,0 +1,14 @@
+<?php
+
+$requestHeaders = getallheaders();
+$requestBody=json_decode(file_get_contents('php://input'));
+
+try{
+    include('../../controller/resposta.php');
+    execute_action("READ_RESPOSTA", $requestHeaders, $requestBody);
+}catch(PDOException $e) {
+    http_response_code(400);
+    echo json_encode("error",$e->getMessage());
+}
+
+?>
