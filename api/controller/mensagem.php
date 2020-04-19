f734bfcb9ce500907ce3b8010334f1cd02ce8b0d
matsales <50926779+matsales@users.noreply.github.com>
2020-04-19 18:44:39 -0300

    atualização controller/mensagem.php

--- api/controller/mensagem.php
+++ api/controller/mensagem.php
@@ -2,16 +2,16 @@
     function execute_action($acao,$requestBody,$requestHeader){
 
         if(!empty($acao)&&!empty($requestHeader)&&!empty($requestBody)){
-            
+
             switch($acao){
                 case "GET_MENSAGENS":
-                    include '../Models/mensagem.php';
+                    include '../../Models/mensagem.php';
                     $mensagem=new  Mensagem();
                     $mensagem->lista();
                     return;
                 break;
                 case "CREATE_MENSAGEM":
-                    include '../Models/mensagem.php';
+                    include '../../Models/mensagem.php';
                     $mensagem=new Mensagem();
                     $mensagem->setMensagem($requestBody->mensagem);
                     $mensagem->setTexto($requestBody->texto);
@@ -20,14 +20,14 @@
                     return;
                 break;
                 case "READ_MENSAGEM":
-                    include "../Models/mensagem.php";
+                    include "../../Models/mensagem.php";
                     $mensagem=new Mensagem();
                     $mensagem->setIdMensagem($requestBody->idMensagem);
                     $mensagem->read();
                     return;
                 break;
                 case "UPDATE_MENSAGEM":
-                    include "../Models/mensagem.php";
+                    include "../../Models/mensagem.php";
                     $mensagem=new Mensagem();
                     $mensagem->setIdMensagem($requestBody->idMensagem);
                     $mensagem->setMensagem($requestBody->mensagem);
@@ -37,7 +37,7 @@
                     return;
                 break;
                 case "DELETE_MENSAGEM":
-                    include "../Models/mensagem.php";
+                    include "../../Models/mensagem.php";
                     $mensagem=new Mensagem();
                     $mensagem->setIdMensagem($requestBody->idMensagem);
                     $mensagem->delete();
@@ -48,6 +48,7 @@
                     echo "Operação inválida";
                     return;
             }
-        }
+
     }
+
 ?>
