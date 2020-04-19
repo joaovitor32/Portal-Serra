6ad14a6755713f5417c29f7f8f13e80cc1e893dd
matsales <50926779+matsales@users.noreply.github.com>
2020-04-19 18:53:27 -0300

    correção em api/routes/users/index.php

--- api/routes/users/index.php
+++ api/routes/users/index.php
@@ -5,7 +5,7 @@ $requestBody=json_decode(file_get_contents('php://input'));
 
 try{
     include('../../controller/users.php');
-    execute_action("GET_USERS", $requestHeaders, $requestBody);
+    execute_action("GET_USERS", $requestBody, $requestHeaders);
 }catch(PDOException $e) {
     http_response_code(400);
     echo json_encode("error",$e->getMessage());
