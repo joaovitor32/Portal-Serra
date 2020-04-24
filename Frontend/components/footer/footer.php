f45fa8b89b150c23b5065542fd1611a779393b67
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-04-24 09:35:47 -0300

    Some adjustments on component Footer

--- Frontend/components/footer/footer.php
+++ Frontend/components/footer/footer.php
@@ -1,38 +1,38 @@
 <footer>
     <div class="container">
         <div class="container-row">
-            <div style="margin-left:1rem"  class="box">
-               <h4>Emails:</h4>
-               <div class="box-emails">
+            <div style="margin-left:1rem" class="box">
+                <h4>Emails:</h4>
+                <div class="box-emails">
                     <h5>Presidente: presidente@serrajr.eng.br</h5>
                     <h5>Operações: operacoes@serrajr.eng.br</h5>
                     <h5>Comercial: comercial@serrajr.eng.br</h5>
                     <h5>Mecânica: projetosmecanica@serrajr.eng.br</h5>
                     <h5>Computação: projetoscomputacao@serrajr.eng.br</h5>
-               </div>
-            </div>
-            <div style="margin-left:1rem"  class="box">
-                <h4>Links:</h4>
-                <div class="box-links">
-                    <a href="https://serrajr.eng.br/" target="_blank"><img class="icon-link" src="../../components/icons/serra.png"  /></a>
-                    <a href="http://www.riojunior.com.br/" target="_blank"><img class="icon-link" src="../../components/icons/riojunior.png"  /></a>
-                    <a href="http://www.riojunior.com.br/" target="_blank"><img class="icon-link" src="../../components/icons/brasil_burned.png"  /></a>
                 </div>
             </div>
-            <div style="margin-left:1rem"  class="box">
+            <div style="margin-left:1rem" class="box">
+                <h4>Links:</h4>
+                <div class="box-links">
+                    <a href="https://serrajr.eng.br/" target="_blank"><img class="icon-link" src="../../components/icons/serra.png" /></a>
+                    <a href="http://www.riojunior.com.br/" target="_blank"><img class="icon-link" src="../../components/icons/riojunior.png" /></a>
+                    <a href="http://www.riojunior.com.br/" target="_blank"><img class="icon-link" src="../../components/icons/brasil_burned.png" /></a>
+                </div>
+            </div>
+            <div style="margin-left:1rem" class="box">
                 <h4>Informações:</h4>
                 <div class="box-informacoes">
-                    <p><img class="icon" src="../../components/icons/home-run.svg"  alt="icon" /><span>Rua Bonfim n°25 - Vila Amelia, Nova Friburgo - RJ, 28625-570</span></p>
-                    <p><img class="icon" src="../../components/icons/phone.svg"  alt="icon" /><span>(22) 2533-2265</span></p>
+                    <p><img class="icon" src="../../components/icons/home-run.svg" alt="icon" /><span>Rua Bonfim n°25 - Vila Amelia, Nova Friburgo - RJ, 28625-570</span></p>
+                    <p><img class="icon" src="../../components/icons/phone.svg" alt="icon" /><span>(22) 2533-2265</span></p>
                 </div>
             </div>
         </div>
     </div>
     <div class="container-footer-bottom">
         <div class="row-footer-bottom">
-            <a href="https://www.facebook.com/serrajreng/" target="_blank"><img class="icon-link-bottom" src="../../components/icons/facebook.svg"  /></a>
-            <a href="https://www.instagram.com/serrajreng/?hl=pt" target="_blank"><img class="icon-link-bottom" src="../../components/icons/instagram.svg"  /></a>
-            <a href="https://www.linkedin.com/company/serra-jr--engenharia/about/" target="_blank"><img class="icon-link-bottom" src="../../components/icons/linkedin.svg"  /></a>
+            <a href="https://www.facebook.com/serrajreng/" target="_blank"><img class="icon-link-bottom" src="../../components/icons/facebook.svg" /></a>
+            <a href="https://www.instagram.com/serrajreng/?hl=pt" target="_blank"><img class="icon-link-bottom" src="../../components/icons/instagram.svg" /></a>
+            <a href="https://www.linkedin.com/company/serra-jr--engenharia/about/" target="_blank"><img class="icon-link-bottom" src="../../components/icons/linkedin.svg" /></a>
         </div>
     </div>
 </footer>
