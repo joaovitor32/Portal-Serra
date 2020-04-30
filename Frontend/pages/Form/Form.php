94b05a631acbe95124f898df7ac67c2000e22a60
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-04-29 22:27:44 -0300

    Changes on page Form

--- Frontend/pages/Form/Form.php
+++ Frontend/pages/Form/Form.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <link rel='stylesheet' type='text/css' href='../../components/UI/footer/footer.css'>
+    <link rel='stylesheet' type='text/css' href='../../components/UI/LoadingSpinner/LoadingSpinner.css'>
     <!--<link rel='stylesheet' type='text/css' href='../../components/UI/form/styles.css'>--->
     <!---<link rel='stylesheet' type='text/css' href='../../components/UI/barra-de-navegacao/barra-de-navegacao.css'>-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
@@ -13,16 +14,14 @@
 </head>
 
 <body style="background-color:rgba(169,169,169,0.7)">
-    <?php
-        require("../../components/UI/barra-de-navegacao/barra-de-navegacao.html")
-    ?>
-    <?php
-        require("../../components/UI/form/form.php")
-    ?>
-    <?php
+    <div id="box-loading-spinner"></div>
+    <div id="box-content">
+        <?php
         require("../../components/UI/footer/footer.php")
-    ?>
-    <script type="text/javascript" src="./Form.js"></script>
+        ?>
+    </div>
+    <script type="text/javascript" src="../GeneralJavascript/GlobalVariables.js"></script>
+    <script type="text/javascript" src="../GeneralJavascript/CheckValidity.js"></script>
 </body>
 
 </html>
