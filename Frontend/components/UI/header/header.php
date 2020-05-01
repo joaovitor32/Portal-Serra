bf59301a7c1c5c0e283f7e0f45c7d942c3199841
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-05-01 15:44:47 -0300

    Mudanças no component UI header

--- Frontend/components/UI/header/header.php
+++ Frontend/components/UI/header/header.php
@@ -1,8 +1,8 @@
 <!--- Os caminhos das imagens são  relativas server side do php ------>
 		<div class="header-grid">
 			<div class="header-item-1">
-				<a href="" class="link-img-serra-jr-logo">	
-					<img src="../../components/icons/serra-jr-logo.png" alt="Logo da Serra Jr." class="img-serra-jr-logo">
+				<a href='' class="link-img-serra-jr-logo">	
+					<img src="../../../components/icons/serra-jr-logo.png" alt="Logo da Serra Jr." class="img-serra-jr-logo">
 				</a>
 			</div>
 			<div class="header-item-2"></div>
@@ -12,17 +12,17 @@
 					<a href="" class="blog-link">Projetos</a>
 				</div>
 				<div class="header-subitem-3-2">
-					<a href="" class="blog-link">História</a>
+					<a href="../../../pages/BlogPage/Historia/Historia.php" class="blog-link">História</a>
 				</div>
 				<div class="header-subitem-3-3">
-					<a href="" class="blog-link">Blog</a>
+					<a href="../../../pages/BlogPage/Index/BlogPage.php" class="blog-link">Blog</a>
 				</div>
 			</div>
 
 			<div class="header-item-4">
-				<a href="" class="link-img-login">
-					<img src="../../components/icons/login.png" alt="Ícone de login" class="img-login">
-					<img src="../../components/icons/login-hover.png" alt="Ícone de login colorido" class="img-login-hover">
+				<a href="../../../pages/Index/Index.php" class="link-img-login">
+					<img src="../../../components/icons/login.png" alt="Ícone de login" class="img-login">
+					<img src="../../../components/icons/login-hover.png" alt="Ícone de login colorido" class="img-login-hover">
 				</a>
 			</div>
 		</div>
