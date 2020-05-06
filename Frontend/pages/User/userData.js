2e89ed9989012e7b35cacffb0cf3ffcf4471a225
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-05-05 21:50:55 -0300

    Mudanças na página de user

--- Frontend/pages/User/userData.js
+++ Frontend/pages/User/userData.js
@@ -1,3 +1,14 @@
+const display=(number)=>{
+    let section=document.getElementsByClassName('section');
+    for(i=0;i<section.length;++i){
+        if(i==number){
+            section[i].style.display="inline";
+        }else{
+            section[i].style.display="none";
+        }
+    }
+}
+
 const fetchUser = async (tok) => {
     try {
         const response = await fetch(url + "users/read.php", {
@@ -8,10 +19,22 @@ const fetchUser = async (tok) => {
             body:null
         })
         let user =await response.json();
-       
+        putData(user);
     }catch(err){
         errModal(err)
     }
+}
+
+const putData=(user)=>{
+    let name=document.getElementById('name')
+    let entrada=document.getElementById('entrada');
+    let cargo=document.getElementById('cargo')
+
+    let entradaDate=new Date(user.entrada);
+    
+    name.innerHTML=user.full_name
+    entrada.innerHTML=(entradaDate.getDate()+1)+"/"+(entradaDate.getMonth()+1)+"/"+entradaDate.getFullYear();
+    cargo.innerHTML=user.cargo
 }
 
 window.onload = () => {
--- Frontend/pages/User/userData.php
+++ Frontend/pages/User/userData.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <link rel='stylesheet' type='text/css' href='../../components/UI/Modal/Modal.css'>
+    <link rel='stylesheet' type='text/css' href='../../components/UI/LoadingSpinner/LoadingSpinner.css'>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@700&display=swap" rel="stylesheet">
     <link rel='stylesheet' type='text/css' href='../../components/UI/footer/footer.css'>
     <link rel='stylesheet' type='text/css' href='../../components/UI/header-sistema/header-sistema.css'>
-    <link rel='stylesheet' type='text/css' href='../../components/UI/LoadingSpinner/LoadingSpinner.css'>
     <!--<link rel='stylesheet' type='text/css' href='../../components/UI/form/styles.css'>--->
     <!---<link rel='stylesheet' type='text/css' href='../../components/UI/barra-de-navegacao/barra-de-navegacao.css'>-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
@@ -18,23 +18,54 @@
 
 <body style="background-color:rgba(169,169,169,0.7)">
     <div id="box-loading-spinner"></div>
-    <?php
-    require("../../components/UI/Modal/Modal.html")
-    ?>
     <div id="box-content">
         <?php
         require("../../components/UI/header-sistema/header-sistema.php")
         ?>
-
+        <div id="full-wrapper">
+            <div  id="content-user">
+                <div class="section" >
+                    <img id="logo-user-content" src="../../components/icons/men.svg" />
+                    <div class="user-data">
+                        <p>Nome:</p>
+                        <p id="name"></p>
+                    </div>
+                    <div class="user-data">
+                        <p>Entrada:</p>
+                        <p id="entrada"></p>
+                    </div>
+                    <div class="user-data">
+                        <p>Cargo:</p>
+                        <p id="cargo"></p>
+                    </div>
+                </div>
+                <div class="section" id="update-user">
+                    update-user
+                </div>
+                <div class="section" id="new-user">
+                    new user
+                </div>
+            </div>
+            <div id="box-actions">
+                <div id="wrapper-actions">
+                    <img onclick="display(0)" src="../../components/icons/exchanging.svg" />
+                    <img onclick="display(1)" src="../../components/icons/new.svg" />
+                    <img onclick="display(2)" src="../../components/icons/eye.svg" />
+                </div>
+            </div>
+        </div>
         <?php
         require("../../components/UI/footer/footer.php")
         ?>
-    </div>
-    <script type="text/javascript" src="../../components/UI/Modal/Modal.js"></script>
-    <script type="text/javascript" src="../GeneralJavascript/CheckValidity.js"></script>
-    <script type="text/javascript" src="../../components/UI/header-sistema/header-sistema.js"></script>
-    <script type="text/javascript" src="../GeneralJavascript/GlobalVariables.js"></script>
-    <script type="text/javascript" src="./userData.js"></script>
+
+        <?php
+        require("../../components/UI/Modal/Modal.html")
+        ?>
+        <script type="text/javascript" src="../../components/UI/Modal/Modal.js"></script>
+        <script type="text/javascript" src="../GeneralJavascript/GlobalVariables.js"></script>
+        <script type="text/javascript" src="../GeneralJavascript/CheckValidity.js"></script>
+        <script type="text/javascript" src="./userData.js"></script>
+        <script type="text/javascript" src="../../components/UI/header-sistema/header-sistema.js"></script>
 </body>
 
 </html>
